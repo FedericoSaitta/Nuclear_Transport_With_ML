09c7d077725e6ee9f588cb6e44be5e63f42e00f7
FedericoSaitta <federico03.saitta@gmail.com>
2025-09-29 13:40:19 +0000

    Adding OpenMC Submodule -Fede

---
+++ test.py
@@ -0,0 +1,15 @@
+import openmc
+
+print("OpenMC version:", openmc.__version__)
+
+material = openmc.Material(name="Empty")
+print("Material created:", material)
+
+geometry = openmc.Geometry()
+cell = openmc.Cell(name="Empty cell")
+geometry.root = cell
+print("Geometry object created:", geometry)
+
+print("OpenMC Python API works")
+
+
